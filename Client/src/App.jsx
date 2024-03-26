45f52fd37c6a5a83b99054b050848f1e7dbae615
Ronit-Kumar-05 <2005686@kiit.ac.in>
2024-03-26 13:07:51 +0530

    create pages and route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
-export default function App() {
-  return <div className="text-3xl text-red-400s">APP</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Sign-In" element={<Signin />} />
+        <Route path="/Sign-Up" element={<SignUp />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
